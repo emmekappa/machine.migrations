c18ce738c27767cf2febc2dca62e56497f96afd8
emmekappa <emmekappa@gmail.com>
2010-01-20 23:02:54 +0100

    Updated sample migrations.

---
+++ Source/Migration.Samples/002_CreateRolesTableAndRelationshipWithUsers.cs
@@ -0,0 +1,27 @@
+using System;
+using Machine.Migrations;
+
+public class CreateRolesTableAndRelationshipWithUsers : FluentMigration
+{
+	public override void Up()
+	{
+		Schema.CreateTable("Roles", builder =>
+		                            	{
+		                            		builder.AddPrimaryKey<Guid>("Id");
+		                            		builder.AddColumn<string>("Name");
+		                            	});
+		SimpleSchema.AddColumn("Users", "RoleId", typeof (Guid), true);
+		SimpleSchema.AddForeignKeyConstraint("Users", "FK_Users_Roles", "RoleId", "Roles", "Id");
+		Database.ExecuteNonQuery("INSERT INTO Users (Name) VALUES ('{0}')", new[] {"Admin"});
+		//Schema.AlterTable("Users", builder => builder.AddColumn<string>("RoleId"));
+		//Schema.AlterTable("Users", builder => builder.AddForeignKey("RoleId",
+		//                                                            new TableInfo("Roles", "Id", ColumnType.Guid, null)));
+	}
+
+	public override void Down()
+	{
+		SimpleSchema.DropConstraint("Users", "FK_Users_Roles");
+		SimpleSchema.DropTable("Roles");
+		SimpleSchema.RemoveColumn("Users", "RoleId");
+	}
+}
