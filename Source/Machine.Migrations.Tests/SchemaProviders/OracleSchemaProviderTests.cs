68168243567a816b229e2a22b1c78c9ea6c6402a
emmekappa <emmekappa@gmail.com>
2010-01-25 16:43:09 +0100

    Added OracleSchemaProviderTests.

---
+++ Source/Machine.Migrations.Tests/SchemaProviders/OracleSchemaProviderTests.cs
@@ -0,0 +1,36 @@
+using Machine.Migrations.DatabaseProviders;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Machine.Migrations.SchemaProviders
+{
+	[TestFixture]
+	public class OracleSchemaProviderTests
+	{
+		private IDatabaseProvider databaseProviderStub;
+		private OracleSchemaProvider oracleSchemaProvider;
+
+		[SetUp]
+		public void SetUp()
+		{
+			databaseProviderStub = MockRepository.GenerateStub<IDatabaseProvider>();
+			oracleSchemaProvider = new OracleSchemaProvider(databaseProviderStub);
+		}
+
+		[Test]
+		public void RenameTable_should_generate_a_valid_DDL_statement()
+		{
+			oracleSchemaProvider.RenameTable("TableName", "NewTableName");
+			databaseProviderStub.AssertWasCalled(x => 
+				x.ExecuteNonQuery("ALTER TABLE \"{0}\" RENAME TO \"{1}\"", "TableName", "NewTableName"));
+		}
+
+		[Test]
+		public void RenameColumn_should_generate_a_valid_DDL_statement()
+		{
+			oracleSchemaProvider.RenameColumn("TableName", "ColumnName", "NewColumnName");
+			databaseProviderStub.AssertWasCalled(x =>
+				x.ExecuteNonQuery("ALTER TABLE \"{0}\" RENAME COLUMN \"{1}\" TO \"{2}\"", "TableName", "ColumnName", "NewColumnName"));
+		}
+	}
+}
